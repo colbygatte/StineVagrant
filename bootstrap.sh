15f0f1aa14049466130f8347a5232a79ed06afd4
colbygatte <colbygatte@gmail.com>
2017-02-16 12:23:15 -0600

    First commit.

---
+++ bootstrap.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+SAMPLE_DATA=$1
+MAGE_VERSION="1.9.1.0"
+DATA_VERSION="1.9.0.0"
+
+# Update Apt
+# --------------------
+apt-get update
+
+# Install Apache & PHP
+# --------------------
+apt-get install -y apache2
+
+sudo apt-get -y update
+sudo add-apt-repository ppa:ondrej/php
+sudo apt-get -y update
+sudo apt-get install -y php7.0 libapache2-mod-php7.0 php7.0 php7.0-common php7.0-gd php7.0-mysql php7.0-mcrypt php7.0-curl php7.0-intl php7.0-xsl php7.0-mbstring php7.0-zip php7.0-bcmath php7.0-iconv
+
+
+# Delete default apache web dir and symlink mounted vagrant dir from host machine
+# --------------------
+rm -rf /var/www/html
+mkdir /vagrant/httpdocs
+ln -fs /vagrant/httpdocs /var/www/html
+
+# Replace contents of default Apache vhost
+# --------------------
+VHOST=$(cat <<EOF
+NameVirtualHost *:8080
+Listen 8080
+<VirtualHost *:80>
+  DocumentRoot "/var/www/html"
+  ServerName localhost
+  <Directory "/var/www/html">
+    AllowOverride All
+  </Directory>
+</VirtualHost>
+<VirtualHost *:8080>
+  DocumentRoot "/var/www/html"
+  ServerName localhost
+  <Directory "/var/www/html">
+    AllowOverride All
+  </Directory>
+</VirtualHost>
+EOF
+)
+
+echo "$VHOST" > /etc/apache2/sites-enabled/000-default.conf
+
+a2enmod rewrite
+service apache2 restart
+
+# Mysql
+# --------------------
+# Ignore the post install questions
+export DEBIAN_FRONTEND=noninteractive
+# Install MySQL quietly
+apt-get -q -y install mysql-server
+
+
+
+
